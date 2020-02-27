ee3a9268241d5a5da8e4293b836645975c9ecd3b
Max Herz <maxopie@gmail.com>
2020-02-27 18:49:15 -0500

    error handling, handling playlists of over 100 songs

--- request.py
+++ request.py
@@ -14,28 +14,45 @@ def get_token():
     token = 'Bearer ' + r.json()['access_token']
     return token
 
+#given a url, gets the spotify playlist id
 def get_playlist_id():
     #get the playlist url using regex
     playlist_url = input("Please enter a public spotify playlist url\n")
     id = re.search("/playlist/([a-zA-Z0-9]*)", playlist_url)
+    if id==None: 
+        print("Bad Playlist URL")
+        exit(-1)
+
     playlist_id = id.group()[10::]
     return playlist_id
 
-def get_album_tracks(songs, token, playlist_id, offset=0):
-    #send a request to get all tracks from the playlist
+#gets all songs from playlist_id, using token as authentication, loading into the dict 'songs'
+def get_album_tracks(songs, token, playlist_id):
+
+    #send a request to get all tracks from the playlist_id
     URL = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     content = "application/json"
     PARAMS = {'Accept':content, 'Content-Type':content, 'Authorization':token} 
-    r = requests.get(url = URL, headers = PARAMS) 
+    offset = 0
+
+    r = requests.get(url = URL, headers = PARAMS, params={"offset":str(offset)}) 
 
     #payload of the http response
     data = r.json()
 
-    #load into dict
-    for item in data["items"]:
-        songs[item["track"]["name"]] = {"id":item["track"]["id"]}
-        print(">> " + item["track"]["name"])
+    #if not items, bad request
+    if "items" not in data:
+        print("Bad HTTP Request")
+        exit(-2)
 
+    #continue to request more songs until we run out
+    while(("items" in data) and len(data["items"])>0):
+        for item in data["items"]:
+            songs[item["track"]["name"]] = {"id":item["track"]["id"]}
+
+        offset+=100
+        r = requests.get(url = URL, headers = PARAMS, params={"offset":str(offset)}) 
+        data = r.json()
 
 
 def main():
@@ -44,10 +61,10 @@ def main():
 
     token = get_token()
     playlist_id = get_playlist_id()
-    get_album_tracks(songs, token, playlist_id)
 
-    #if the playlist is over 100 songs, keep track of how many are left
-    #songs_left = int(data["total"]) - len(data["items"])
+    get_album_tracks(songs, token, playlist_id)
+    print(len(songs))
+
 
 if __name__ == "__main__":
     main()
