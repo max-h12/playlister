ad79886d8a4bddde34dfd48ec6a8e8484d682183
Max Herz <maxopie@gmail.com>
2020-02-27 13:35:31 -0500

    Request now uses client id instead of expired one-time token

--- request.py
+++ request.py
@@ -1,7 +1,17 @@
 import requests
+import base64
 import re
-import key 
 
+import key
+
+#get an auth key from spotify using client id
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+PARAMS = {'grant_type':'client_credentials'}
+
+r = requests.post(url = TOKEN_URL, data = PARAMS, auth = (key.client_id, key.client_secret))
+token = 'Bearer ' + r.json()['access_token']
+
+#get the playlist url
 playlist_url = input("Please enter a public spotify playlist url\n")
 id = re.search("/playlist/([a-zA-Z0-9]*)", playlist_url)
 playlist_id = id.group()[10::]
@@ -9,7 +19,7 @@ playlist_id = id.group()[10::]
 URL = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 content = "application/json"
 
-PARAMS = {'Accept':content, 'Content-Type':content, 'Authorization':key.auth} 
+PARAMS = {'Accept':content, 'Content-Type':content, 'Authorization':token} 
   
 # sending get request and saving the response as response object 
 r = requests.get(url = URL, headers = PARAMS) 
@@ -19,4 +29,4 @@ data = r.json()
 
 print("Tracks Found:")
 for item in data["items"]:
-    print(item["track"]["name"])
+    print(">> "+item["track"]["name"])
