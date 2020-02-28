73a413cfe1448cb7c7258ed5b2c6fdba851ecdd1
Max Herz <maxopie@gmail.com>
2020-02-28 14:21:17 -0500

    Full output format

--- request.py
+++ request.py
@@ -1,14 +1,16 @@
 import requests
-import base64
 import re
 
 import key
 
+PITCH_MAP = {0:"C", 1:"C#", 2:"D", 3:"D#", 4:"E", 5:"F", 6:"F#", 7:"G", 8:"G#", 9:"A", 10:"A#", 11:"B"}
+MODE_MAP = {0:"Minor", 1:"Major"}
+
 #get an auth key from spotify using client id
 def get_token():
-    TOKEN_URL = "https://accounts.spotify.com/api/token"
-    PARAMS = {'grant_type':'client_credentials'}
-    r = requests.post(url = TOKEN_URL, data = PARAMS, auth = (key.client_id, key.client_secret))
+    request_url = "https://accounts.spotify.com/api/token"
+    params = {'grant_type':'client_credentials'}
+    r = requests.post(url = request_url, data = params, auth = (key.client_id, key.client_secret))
 
     #auth token for all subsequent requests
     token = 'Bearer ' + r.json()['access_token']
@@ -30,12 +32,12 @@ def get_playlist_id():
 def get_album_tracks(songs, token, playlist_id):
 
     #send a request to get all tracks from the playlist_id
-    URL = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    request_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     content = "application/json"
-    PARAMS = {'Accept':content, 'Content-Type':content, 'Authorization':token} 
+    params = {'Accept':content, 'Content-Type':content, 'Authorization':token} 
     offset = 0
 
-    r = requests.get(url = URL, headers = PARAMS, params={"offset":str(offset)}) 
+    r = requests.get(url = request_url, headers = params, params={"offset":str(offset)}) 
 
     #payload of the http response
     data = r.json()
@@ -51,31 +53,55 @@ def get_album_tracks(songs, token, playlist_id):
             songs[item["track"]["id"]] = {"name":item["track"]["name"]}
             
         offset+=100
-        r = requests.get(url = URL, headers = PARAMS, params={"offset":str(offset)}) 
+        r = requests.get(url = request_url, headers = params, params={"offset":str(offset)}) 
         data = r.json()
 
+#gets all the features for each song in the songs map
 def get_song_features(songs, attribute_avg, token):
     #setup the request
-    URL = "https://api.spotify.com/v1/audio-features/"
-    PARAMS = {'Authorization':token} 
+    request_url = "https://api.spotify.com/v1/audio-features/"
+    params = {'Authorization':token} 
 
     #go through every song
     for id in songs:
         #request the attributes for that song
-        r = requests.get(url = URL + id, headers = PARAMS) 
+        r = requests.get(url = request_url + id, headers = params) 
         data = r.json()
 
         #add each attribute into the map and add its partial average in
         for key in attribute_avg:
             songs[id][key] = data[key]
-            attribute_avg[key] += (data[key]/len(songs))
+            attribute_avg[key] += ((data[key])/(len(songs)))
+
+#formats the attribute averages for display to user
+def format_oput(attribute_avg):
+    milli = attribute_avg['duration_ms']
+    seconds = round(int((milli/1000)%60),2)
+    minutes = round(int((milli/(1000*60))%60),2)
+    print(f"Average duration: {minutes}:{seconds}")
+    est_pitch = round(attribute_avg['key'])
+    if(PITCH_MAP.__contains__(est_pitch)):
+        print(f"Overall key average: {PITCH_MAP[est_pitch]}")
+    else:
+        print("Overall key average: Unknown")
+    print(f"Major/Minor Average: More {MODE_MAP[round(attribute_avg['mode'])]}")
+    print(f"Average Time Signature (beats per bar/measure): {round(attribute_avg['time_signature'])}")
+    print(f"Average Acousticness (confidence a track is acoustic)[0.0 - 1.0]: {round(attribute_avg['acousticness'],3)}")
+    print(f"Average Danceability [0.0 - 1.0]: {round(attribute_avg['danceability'],3)}")
+    print(f"Average Energy [0.0 - 1.0]: {round(attribute_avg['energy'],3)}")
+    print(f"Average Instrumentalness (confidence there are no words) [0.0 - 1.0]: {round(attribute_avg['instrumentalness'],3)}")
+    print(f"Average Liveness (confidence song was recorded in front of an audience) [0.0 - 1.0]: {round(attribute_avg['liveness'],3)}")
+    print(f"Average Loudness [-60db - 0db]: {round(attribute_avg['loudness'],3)}db")
+    print(f"Average Speechiness [0.0 - 1.0]: {round(attribute_avg['speechiness'],3)}")
+    print(f"Average Valence (how cheerful the track is) [0.0 - 1.0]: {round(attribute_avg['valence'],3)}")
+    print(f"Average Tempo (BPM): {round(attribute_avg['tempo'],3)}")
 
 def main():
     #map of songs, key being song-title, value is a map of attributes
     songs = {}
 
     #average of all songs in respective categories
-    attribute_avg = {"duration_ms":0, "key":0, "mode":0, "acousticness":0, "danceability":0, "energy":0, "instrumentalness":0, "liveness":0, "loudness":0, "speechiness":0, "valence":0, "tempo":0}
+    attribute_avg = {"duration_ms":0, "key":0, "mode":0, "time_signature":0, "acousticness":0, "danceability":0, "energy":0, "instrumentalness":0, "liveness":0, "loudness":0, "speechiness":0, "valence":0, "tempo":0}
 
     #get authorization and playlist id from input
     token = get_token()
@@ -84,6 +110,9 @@ def main():
     #get the songs, then load in their features
     get_album_tracks(songs, token, playlist_id)
     get_song_features(songs, attribute_avg, token)
+
+    print(f"Songs found in playlist: {len(songs)}")
+    format_oput(attribute_avg)
 
 if __name__ == "__main__":
     main()
